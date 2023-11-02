daf954c5beb8b968c292b2803522771825eab671
Kantita01 <kantita5069@gmail.com>
2023-11-02 14:48:58 +0700

    first commit

---
+++ app2.js
@@ -0,0 +1,4 @@
+const message = "Hello";
+message = "Hi"
+console.log(message);
+document.getElementById("id").innerHTML = message;
---
+++ app4.js
@@ -0,0 +1,7 @@
+var apple = 10
+{
+    console.log(apple) //
+    document.getElementById("app").innerHTML = apple;
+}
+console.log(apple + 10)//
+document.getElementById("app").innerHTML = apple + 10;
